c0f6e6a1063d88739e226fc675207426cc00323e
SuperTurtleWave <84501830+SuperTurtleWave@users.noreply.github.com>
2021-06-19 18:31:05 -0700

    Add files via upload

---
+++ tests/test_qualifier.py
@@ -0,0 +1,42 @@
+# Import pathlib
+from pathlib import Path
+
+#Import fileio
+from qualifier.utils import fileio
+
+# Import Calculators
+from qualifier.utils import calculators
+
+# Import Filters
+from qualifier.filters import credit_score
+from qualifier.filters import debt_to_income
+from qualifier.filters import loan_to_value
+from qualifier.filters import max_loan_size
+
+def test_save_csv():
+    csvpath = Path('./data/qualifying_loans_test.csv')
+    loans = "test"
+    fileio.save_csv(csvpath, loans)
+    assert csvpath.exists()
+
+def test_calculate_monthly_debt_ratio():
+    assert calculators.calculate_monthly_debt_ratio(1500, 4000) == 0.375
+
+def test_calculate_loan_to_value_ratio():
+    assert calculators.calculate_loan_to_value_ratio(210000, 250000) == 0.84
+
+def test_filters():
+    bank_data = fileio.load_csv(Path('./data/daily_rate_sheet.csv'))
+    current_credit_score = 750
+    debt = 1500
+    income = 4000
+    loan = 210000
+    home_value = 250000
+
+    monthly_debt_ratio = 0.375
+
+    loan_to_value_ratio = 0.84
+
+    bank_data_filtered = credit_score.filter_credit_score(current_credit_score,bank_data)
+    print(bank_data_filtered)
+    assert len(bank_data_filtered)==10
